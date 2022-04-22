586471574629cc7c2127990cf2421952df0d6b2e
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-04-22 09:49:29 +0300

    Edit App JS

--- src/App.js
+++ src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <div
-        style={{
-          backgroundColor: 'black',
-          width: '35%',
-          margin: 'auto',
-          marginTop: 100,
-        }}
-      >
-        <Calculator />
-      </div>
+const App = () => (
+  <div className="App">
+    <div
+      style={{
+        backgroundColor: 'black',
+        width: '35%',
+        margin: 'auto',
+        marginTop: 100,
+      }}
+    >
+      <Calculator />
     </div>
-  );
-}
-
+  </div>
+);
 export default App;
