935013bbefebdf899b9045cbf746179a283b024a
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-04-22 10:14:02 +0300

    Add App JS Class

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 
 import Calculator from './components/Calculator';
 
-const App = () => (
-  <div className="App">
-    <div
-      style={{
-        backgroundColor: 'black',
-        width: '35%',
-        margin: 'auto',
-        marginTop: 100,
-      }}
-    >
-      <Calculator />
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div
+          style={{
+            backgroundColor: 'black',
+            width: '35%',
+            margin: 'auto',
+            marginTop: 100,
+          }}
+        >
+          <Calculator />
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
